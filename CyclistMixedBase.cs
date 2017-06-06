54da8ec4778cf33bf329a938c124d9acd61b59eb
mihaisoldan <catalinmihai.soldan@studenti.unicam.it>
2017-06-06 05:32:53 +0300

    created base class

---
+++ CyclistMixedBase.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using MicrosoftResearch.Infer;
+using MicrosoftResearch.Infer.Models;
+using MicrosoftResearch.Infer.Distributions;
+using MicrosoftResearch.Infer.Maths;
+
+namespace CyclingTime3
+{
+    class CyclistMixedBase{
+        protected InferenceEngine inferenceEngine;
+        protected int numComponents;
+        protected VariableArray<Gaussian> averageTimePriors;
+        protected VariableArray<double> averageTime;
+        protected VariableArray<Gamma> trafficNoisePriors;
+        protected VariableArray<double> trafficNoise;
+
+        //still 2c 
+        protected Variable<Dirichlet> mixingPrior;
+        protected Variable<Vector> mixingCoefficients;
+
+        public virtual void CreateModel(){
+            inferenceEngine = new InferenceEngine(new VariationalMessagePassing());
+            numComponents = 2;
+            Range componentRange = new Range(numComponents);
+            averageTimePriors = Variable.Array<Gaussian>(componentRange);
+            trafficNoisePriors = Variable.Array<Gamma>(componentRange);
+            averageTime = Variable.Array<double>(componentRange);
+            trafficNoise = Variable.Array<double>(componentRange);
+
+            using (Variable.ForEach(componentRange))
+            {
+                averageTime[componentRange] = Variable.Random<double, Gaussian>(averageTimePriors[componentRange]);
+                trafficNoise[componentRange] = Variable.Random<double, Gamma>(trafficNoisePriors[componentRange]);
+            }
+
+            mixingPrior = Variable.New<Dirichlet>();
+            mixingCoefficients = Variable<Vector>.Random(mixingPrior);
+            mixingCoefficients.SetValueRange(componentRange);
+
+
+        }
+
+        public void SetModelData(ModelDataMixed modelData)
+        {
+            averageTimePriors.ObservedValue = modelData.averageTimeDist;
+            trafficNoisePriors.ObservedValue = modelData.trafficNoiseDist;
+            mixingPrior.ObservedValue = modelData.mixingDist;
+        }
+
+        public struct ModelDataMixed
+        {
+            public Gaussian[] averageTimeDist;
+            public Gamma[] trafficNoiseDist;
+            public Dirichlet mixingDist;
+        }
+
+    }
+}
---
+++ Program3.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using MicrosoftResearch.Infer;
+using MicrosoftResearch.Infer.Models;
+using MicrosoftResearch.Infer.Distributions;
+using MicrosoftResearch.Infer.Maths;
+
+namespace CyclingTime3 {
+    class Program3 {
+
+           static void Main(string[] args) {
+        }
+    }
+}
